e7f24be2476fcc48a0b31692ab4a05ae99ae3c44
Sky_tech <oluwolesayo10@gmail.com>
2021-11-30 14:48:13 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+//Paths variables
+const publicFolder = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/Partials')
+
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+app.use(express.static(publicFolder))
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res)=>{
+  res.render('index', {
+    title: 'Home page',
+    name: 'Sayo'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help'
+  })
+})
+
+app.get('/about', (req, res)=>{
+  res.render('about', {
+    title: 'About Page',
+    name: "Yomzy"
+  })
+})
+
+app.get('/weather', (req, res) => {
+  res.send({
+    location: 'Lagos',
+    forecast: 'There is 3.6% chance of rain today'
+  })
+})
+
+
+app.listen(3000, () =>{
+  console.log("Server is running")
+})
